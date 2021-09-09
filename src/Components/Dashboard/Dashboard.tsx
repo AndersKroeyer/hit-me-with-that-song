c67a72eff447683ad72d453f76eeda8ee3926e5a
Anders <afk@it-minds.dk>
2021-09-09 22:41:44 +0200

    test

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,5 +1,10 @@
 function Dashboard() {
-  return <div>This is a nice little app and another sentence</div>;
+  return (
+    <div>
+      This is a nice little app and another sentence
+      <div>another div!</div>
+    </div>
+  );
 }
 
 export default Dashboard;
